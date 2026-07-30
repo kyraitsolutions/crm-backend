57aadb0b7738f5cc8df29d0ecd44eb9df2a61205
abhijeet <abhijeetsingh5631@gmail.com>
2026-07-30 11:53:00 +0530

    update package json file

--- src/workers/email.worker.ts
+++ src/workers/email.worker.ts
@@ -29,106 +29,106 @@ export async function startWorker() {
   }
 }
 function registerProcessors() {
-emailQueue.process("welcome-email", async (job) => {
-  const { email, url } = job.data;
-  logger.info(`Processing welcome email for ${email}`);
-  await emailUtils.sendWelcomeEmail(email, url);
-});
-emailQueue.process("otp-email", async (job) => {
-  const { email, otp } = job.data;
-  logger.info(`Processing otp email for ${email}`);
-  await emailUtils.sendOTPEmail(email, otp);
-});
+  emailQueue.process("welcome-email", async (job) => {
+    const { email, url } = job.data;
+    logger.info(`Processing welcome email for ${email}`);
+    await emailUtils.sendWelcomeEmail(email, url);
+  });
+  emailQueue.process("otp-email", async (job) => {
+    const { email, otp } = job.data;
+    logger.info(`Processing otp email for ${email}`);
+    await emailUtils.sendOTPEmail(email, otp);
+  });
 
-emailQueue.process("account-creation-email", async (job) => {
-  const { accountEmail, accountName } = job.data;
-  logger.info(`Processing welcome email for ${accountEmail}`);
-  await emailUtils.sendAccountCreationEmail(accountEmail, accountName);
-});
+  emailQueue.process("account-creation-email", async (job) => {
+    const { accountEmail, accountName } = job.data;
+    logger.info(`Processing welcome email for ${accountEmail}`);
+    await emailUtils.sendAccountCreationEmail(accountEmail, accountName);
+  });
 
-emailQueue.process("campaign-email", async (job) => {
-  const { to, name, subject, html, fromEmail } = job.data;
+  emailQueue.process("campaign-email", async (job) => {
+    const { to, name, subject, html, fromEmail } = job.data;
 
-  const personalizedHtml = html.replace("{{name}}", name || "there");
-
-  await emailUtils.sendEmail(
-    to,
-    subject,
-    personalizedHtml,
-    undefined,
-    fromEmail,
-  );
-});
-
-emailQueue.process("send-email-activity",async (job) => {
-    const { emailActivityId, to, name, subject, html, fromEmail, } = job.data;
-
-  try {
     const personalizedHtml = html.replace("{{name}}", name || "there");
 
-      // send email
-      const result = await emailUtils.sendEmail(to, subject, personalizedHtml, undefined, fromEmail);
-      console.log("send email result",result)
-      // update DB success
-      console.log("email activity id",emailActivityId);
-      const messageId=result?.messageId??"";
-      console.log("Messageid",messageId)
-      await emailRepository.updateEmailStatus(emailActivityId,"sent",messageId,null);
-
-    } catch (error: any) {
-      console.log("Error:",error)
-
-      // update DB failed
-      await emailRepository.updateEmailStatus(emailActivityId,"faild","",error);
-
-      throw error;
-    }
-});
-
-emailQueue.process(QUEUE_JOBS.LEAD_ACKNOWLEDGEMENT_EMAIL, async (job) => {
-  const { email, lead } = job.data;
-
-  logger.info(`Processing lead acknowledgement email for ${email}`);
-  await emailUtils.sendLeadAcknowledgementEmail(email, lead);
-});
-
-emailQueue.process(QUEUE_JOBS.SEND_ONBOARDING_EMAIL, async (job) => {
-  const {
-    organizationName,
-    firstName,
-    lastName,
-    email,
-    dashboardUrl,
-    supportEmail,
-    createdAt,
-    year,
-  } = job.data;
-  await emailUtils.sendOnboardingSuccessEmail({
-    organizationName,
-    firstName,
-    lastName,
-    email,
-    dashboardUrl,
-    supportEmail,
-    createdAt,
-    year,
+    await emailUtils.sendEmail(
+      to,
+      subject,
+      personalizedHtml,
+      undefined,
+      fromEmail,
+    );
   });
-});
 
-emailQueue.process(QUEUE_JOBS.SEND_TASK_ASSIGNED_EMAIL, async (job) => {
-  const { email, task } = job.data;
-  await emailUtils.sendTaskAssignedEmail(email, task);
-  // await emailUtils.sendEmail(
-  //   email,
-  //   `New Task Assigned: ${task?.taskTitle}`,
-  //   `New Task Assigned: ${task?.taskTitle}`,
-  // );
-});
+  emailQueue.process("send-email-activity",async (job) => {
+      const { emailActivityId, to, name, subject, html, fromEmail, } = job.data;
 
-emailQueue.process(QUEUE_JOBS.SEND_LEAD_ASSIGNED_EMAIL, async (job) => {
-  const { email, lead } = job.data;
-  await emailUtils.sendLeadAssignedEmail(email, lead);
-});
+    try {
+      const personalizedHtml = html.replace("{{name}}", name || "there");
+
+        // send email
+        const result = await emailUtils.sendEmail(to, subject, personalizedHtml, undefined, fromEmail);
+        console.log("send email result",result)
+        // update DB success
+        console.log("email activity id",emailActivityId);
+        const messageId=result?.messageId??"";
+        console.log("Messageid",messageId)
+        await emailRepository.updateEmailStatus(emailActivityId,"sent",messageId,null);
+
+      } catch (error: any) {
+        console.log("Error:",error)
+
+        // update DB failed
+        await emailRepository.updateEmailStatus(emailActivityId,"faild","",error);
+
+        throw error;
+      }
+  });
+
+  emailQueue.process(QUEUE_JOBS.LEAD_ACKNOWLEDGEMENT_EMAIL, async (job) => {
+    const { email, lead } = job.data;
+
+    logger.info(`Processing lead acknowledgement email for ${email}`);
+    await emailUtils.sendLeadAcknowledgementEmail(email, lead);
+  });
+
+  emailQueue.process(QUEUE_JOBS.SEND_ONBOARDING_EMAIL, async (job) => {
+    const {
+      organizationName,
+      firstName,
+      lastName,
+      email,
+      dashboardUrl,
+      supportEmail,
+      createdAt,
+      year,
+    } = job.data;
+    await emailUtils.sendOnboardingSuccessEmail({
+      organizationName,
+      firstName,
+      lastName,
+      email,
+      dashboardUrl,
+      supportEmail,
+      createdAt,
+      year,
+    });
+  });
+
+  emailQueue.process(QUEUE_JOBS.SEND_TASK_ASSIGNED_EMAIL, async (job) => {
+    const { email, task } = job.data;
+    await emailUtils.sendTaskAssignedEmail(email, task);
+    // await emailUtils.sendEmail(
+    //   email,
+    //   `New Task Assigned: ${task?.taskTitle}`,
+    //   `New Task Assigned: ${task?.taskTitle}`,
+    // );
+  });
+
+  emailQueue.process(QUEUE_JOBS.SEND_LEAD_ASSIGNED_EMAIL, async (job) => {
+    const { email, lead } = job.data;
+    await emailUtils.sendLeadAssignedEmail(email, lead);
+  });
 
 }
 
