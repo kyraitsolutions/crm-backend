62b08bc9db1391a2b6279a1ef1205acad1936b60
Hem Bhadur <sushilkc2611@gmail.com>
2026-09-05 13:05:43 +0530

    just commit so that i can take pull from main

--- src/modules/whatsapp/account/controllers/whatsapp.controller.ts
+++ src/modules/whatsapp/account/controllers/whatsapp.controller.ts
@@ -20,22 +20,8 @@ export class WhatsappAccountController {
     const { accountId } = req.params;
     // const orgId = req?.user?.organizationId;
 
-    console.log(this);
-
     const result = await this.service.syncContacts(accountId);
 
     httpResponse(req, res, 200, "Contacts synced successfully", result);
   }
 }
-
-// onUserMessage(convId):
-//   cancelPendingJobs(convId)   // remove existing 20s/4m/10m jobs from BullMQ
-//   scheduleJob(convId, delay=20_000,  stage='nudge_1')
-//   scheduleJob(convId, delay=240_000, stage='nudge_2')
-//   scheduleJob(convId, delay=600_000, stage='nudge_3')
-
-// processJob(convId, stage):
-//   conv = loadConversation(convId)
-//   if conv.status in ['closed', 'human_handoff', 'opted_out']: return
-//   if conv.lastMessageAt > job.scheduledAt: return  // stale, user already replied
-//   sendReengagementMessage(conv, stage)
